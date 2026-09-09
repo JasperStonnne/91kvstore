1ccfcc197d657079867e1db213d0142ab7c852e9
JasperSao <shijingyuan061010@gmail.com>
2026-09-09 13:36:54 +0000

    feat(protocol): support CRLF-delimited command batches
    Split multiple complete commands from one receive buffer, execute them in order, and aggregate their responses.

--- src/kvstore.c
+++ src/kvstore.c
@@ -396,6 +396,53 @@ int kvs_protocol(char *msg,int length,char *response){
     return kvs_filter_protocol(tokens,count,response);
 }
 
+static int kvs_find_crlf(const char *msg,int length){
+    if(msg==NULL||length<2){
+        return -1;
+    }
+    for(int i=0;i+1<length;i++){
+        if(msg[i]=='\r'&&msg[i+1]=='\n'){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int kvs_batch_protocol(char *msg,int length,char *response,int response_capacity){
+    if(msg==NULL||length<=0||response==NULL||response_capacity<=0){
+        return -1;
+    }
+    int request_offset=0;//处理到请求的什么位置
+    int response_offset=0;//当前写入了多少响应
+    while(request_offset<length){
+        char *command_start=msg+request_offset;
+        int remaining_length=length-request_offset;
+        int command_end=kvs_find_crlf(command_start,remaining_length);
+        if(command_end<0){
+            break;
+        }
+        command_start[command_end]='\0';
+        if(command_end==0){
+            return -1;
+        }
+        if (response_offset >= response_capacity) {
+            return -1;
+        }
+        int response_length=kvs_protocol(command_start,command_end,response+response_offset);
+        if (response_length < 0 ||response_length > response_capacity - response_offset) {
+            return -1;
+        }
+        response_offset+=response_length;
+        request_offset+=command_end+2;
+    }
+    return response_offset;
+    
+}
+
+static int kvs_network_protocol(char *msg,int length,char *response){
+    return kvs_batch_protocol(msg,length,response,1024);
+}
+
 int init_kvengine(void){
 
 #if ENABLE_ARRAY
@@ -467,11 +514,11 @@ int main(int argc,char *argv[]){
         return -1;
 }
 #if (NETWORK_SELECT==NETWORK_REACTOR)
-    reactor_start(port,kvs_protocol);
+    reactor_start(port,kvs_network_protocol);
 #elif(NETWORK_SELECT==NETWORK_NTYCO)
-    ntyco_start(port,kvs_protocol);
+    ntyco_start(port,kvs_network_protocol);
 #elif(NETWORK_SELECT==NETWORK_PROACTOR)
-    proactor_start(port,kvs_protocol);
+    proactor_start(port,kvs_network_protocol);
 #endif
 
     dest_kvengine();
